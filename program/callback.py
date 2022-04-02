1654d0d2cac479d16ebbcad35732b81e53419274
jxBkfTjenc <99679586+jxBkfTjenc@users.noreply.github.com>
2022-04-03 03:39:59 +0700

    Update callback.py

--- program/callback.py
+++ program/callback.py
@@ -39,27 +39,19 @@ from config import (
 async def start_set(_, query: CallbackQuery):
     await query.answer("home start")
     await query.edit_message_text(
-        f"""Hi [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) 👋🏻\n
-💭 [{me_bot.first_name}](https://t.me/{me_bot.username}) is a bot to play music and video in groups, through the new Telegram video chats.
-
-🕵🏻 Check out all the **Bot's commands** and how they work by clicking on the » 📚 **Commands** button!
-
-🧑🏻‍💻 To know how to use this bot, please click on the » ❓ **Basic Guide** button!
+        f"""Hai [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) 👋🏻\n
+💭 [{me_bot.first_name}](https://t.me/{me_bot.username}) adalah bot untuk memutar musik dan video dalam grup, melalui obrolan video Telegram.
 """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("➕ Add me to a Group ➕", url=f"https://t.me/{me_bot.username}?startgroup=true")
+                    InlineKeyboardButton("➕ Tambahkan saya ke Grup ➕", url=f"https://t.me/{me_bot.username}?startgroup=true")
                 ],[
-                    InlineKeyboardButton("❓ Basic Guide", callback_data="user_guide")
+                    InlineKeyboardButton("📚 CMD", callback_data="command_list"),
+                    InlineKeyboardButton("❓ Panduan", callback_data="user_guide")
                 ],[
-                    InlineKeyboardButton("📚 Commands", callback_data="command_list"),
-                    InlineKeyboardButton("❤️ Donate", url=f"https://t.me/{OWNER_USERNAME}")
-                ],[
-                    InlineKeyboardButton("👥 Support Group", url=f"https://t.me/{GROUP_SUPPORT}"),
-                    InlineKeyboardButton("📣 Support Channel", url=f"https://t.me/{UPDATES_CHANNEL}")
-                ],[
-                    InlineKeyboardButton("🌐 Source Code", url="https://github.com/levina-lab/video-stream")
+                    InlineKeyboardButton("👥 Group", url=f"https://t.me/{GROUP_SUPPORT}"),
+                    InlineKeyboardButton("📣 Channel", url=f"https://t.me/{UPDATES_CHANNEL}")
                 ],
             ]
         ),
@@ -72,17 +64,17 @@ async def start_set(_, query: CallbackQuery):
 async def quick_set(_, query: CallbackQuery):
     await query.answer("quick bot usage")
     await query.edit_message_text(
-        f"""ℹ️ Quick use Guide bot, please read fully !
+        f"""ℹ️ Panduan penggunaan bot, harap baca sepenuhnya !
 
-👩🏻‍💼 » /play - Type this with give the song title or youtube link or audio file to play Music. (Remember to don't play YouTube live stream by using this command!, because it will cause unforeseen problems.)
+👩🏻‍💼 » /play - Ketik ini dengan memberikan judul lagu atau tautan youtube atau file audio untuk memutar Musik. (Ingat untuk tidak memutar live streaming YouTube dengan menggunakan perintah ini!, karena akan menyebabkan ERROR yang tidak terduga.)
 
-👩🏻‍💼 » /vplay - Type this with give the song title or youtube link or video file to play Video. (Remember to don't play YouTube live video by using this command!, because it will cause unforeseen problems.)
+👩🏻‍💼 » /vplay - Ketik ini dengan memberikan judul lagu atau link youtube atau file video untuk memutar Video. (Ingat untuk tidak memutar video langsung YouTube dengan menggunakan perintah ini!, karena akan menyebabkan ERROR yang tidak terduga.)
 
-👩🏻‍💼 » /vstream - Type this with give the YouTube live stream video link or m3u8 link to play live Video. (Remember to don't play local audio/video files or non-live YouTube video by using this command!, because it will cause unforeseen problems.)
+👩🏻‍💼 » /vstream - Ketik ini dengan memberikan tautan video streaming langsung YouTube atau tautan m3u8 untuk memutar Video langsung. (Ingat untuk tidak memutar file audio/video lokal atau video YouTube non-live dengan menggunakan perintah ini!, karena akan menyebabkan ERROR yang tidak terduga.)
 
-❓ Have questions? Contact us in [Support Group](https://t.me/{GROUP_SUPPORT}).""",
+❓ Ada pertanyaan? Hubungi kami di [Support Group](https://t.me/{GROUP_SUPPORT}).""",
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("🔙 Go Back", callback_data="user_guide")]]
+            [[InlineKeyboardButton("🔙 Kembali", callback_data="user_guide")]]
         ),
         disable_web_page_preview=True,
     )
@@ -93,25 +85,23 @@ async def quick_set(_, query: CallbackQuery):
 async def guide_set(_, query: CallbackQuery):
     await query.answer("user guide")
     await query.edit_message_text(
-        f"""❓ How to use this Bot ?, read the Guide below !
+        f"""❓ Bagaimana cara menggunakan Bot ini?, baca Panduan di bawah ini !
 
-1.) First, add this bot to your Group.
-2.) Then, promote this bot as administrator on the Group also give all permissions except Anonymous admin.
-3.) After promoting this bot, type /reload in Group to update the admin data.
-3.) Invite @{me_user.username} to your group or type /userbotjoin to invite her, unfortunately the userbot will joined by itself when you type `/play (song name)` or `/vplay (song name)`.
-4.) Turn on/Start the video chat first before start to play video/music.
+1.) Pertama, tambahkan bot ini ke Grup Anda.
+2.) Kemudian, jadikan bot ini sebagai administrator di Grup juga berikan semua izin kecuali admin Anonim.
+3.) Setelah bot ini dijadikan admin, ketik /reload di Grup untuk memperbarui data admin.
+3.) Undang @{me_user.username} ke grup Anda atau ketik /userbotjoin untuk mengundangnya, tetapi dengan mengetik `/play (judul lagu)` atau `/vplay (judul lagu)` assistant biasanya juga otomatis bergabung dengan sendirinya.
+4.) Nyalakan /Mulai obrolan video terlebih dahulu sebelum memulai memutar video/musik.
 
-`- END, EVERYTHING HAS BEEN SETUP -`
+📌 Jika assistant bot tidak bergabung ke obrolan video, pastikan obrolan video sudah diaktifkan dan assistant bot ada di obrolan.
 
-📌 If the userbot not joined to video chat, make sure if the video chat already turned on and the userbot in the chat.
-
-💡 If you have a follow-up questions about this bot, you can tell it on my support chat here: @{GROUP_SUPPORT}.""",
+💡 Jika Anda memiliki pertanyaan lanjutan tentang bot ini, Anda dapat menceritakannya di obrolan dukungan saya di sini: @{GROUP_SUPPORT}.""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("» Quick use Guide «", callback_data="quick_use")
+                    InlineKeyboardButton("» Panduan penggunaan cepat «", callback_data="quick_use")
                 ],[
-                    InlineKeyboardButton("🔙 Go Back", callback_data="home_start")
+                    InlineKeyboardButton("🔙 Kembali", callback_data="home_start")
                 ],
             ]
         ),
@@ -124,22 +114,22 @@ async def commands_set(_, query: CallbackQuery):
     user_id = query.from_user.id
     await query.answer("commands menu")
     await query.edit_message_text(
-        f"""✨ **Hello [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) !**
+        f"""✨ **Hallo [{query.message.chat.first_name}](tg://user?id={query.message.chat.id}) !**
 
-» Check out the menu below to read the module information & see the list of available Commands !
+» Lihat menu di bawah ini untuk membaca informasi modul & melihat daftar Perintah yang tersedia !
 
-All commands can be used with (`! / .`) handler""",
+Semua perintah dapat digunakan dengan (`! / .`) """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("👮🏻‍♀️ Admins Commands", callback_data="admin_command"),
+                    InlineKeyboardButton("👮🏻‍♀️ Admins", callback_data="admin_command"),
                 ],[
-                    InlineKeyboardButton("👩🏻‍💼 Users Commands", callback_data="user_command"),
+                    InlineKeyboardButton("👩🏻‍💼 semua pengguna", callback_data="user_command"),
                 ],[
-                    InlineKeyboardButton("Sudo Commands", callback_data="sudo_command"),
-                    InlineKeyboardButton("Owner Commands", callback_data="owner_command"),
+                    InlineKeyboardButton("Sudo", callback_data="sudo_command"),
+                    InlineKeyboardButton("Owner", callback_data="owner_command"),
                 ],[
-                    InlineKeyboardButton("🔙 Go Back", callback_data="home_start")
+                    InlineKeyboardButton("🔙 Kembali", callback_data="home_start")
                 ],
             ]
         ),
@@ -149,24 +139,24 @@ All commands can be used with (`! / .`) handler""",
 @Client.on_callback_query(filters.regex("user_command"))
 @check_blacklist()
 async def user_set(_, query: CallbackQuery):
-    await query.answer("basic commands")
+    await query.answer("Perintah Untuk Semua Pengguna")
     await query.edit_message_text(
-        f"""✏️ Command list for all user.
+        f"""✏️ Daftar perintah untuk semua pengguna.
 
-» /play (song name/youtube link) - play the music from youtube
-» /stream (m3u8/youtube live link) - play youtube/m3u8 live stream music
-» /vplay (video name/youtube link) - play the video from youtube
-» /vstream (m3u8/youtube live link) - play youtube/m3u8 live stream video
-» /playlist - view the queue list of songs and current playing song
-» /lyric (query) - search for song lyrics based on the name of the song
-» /video (query) - download video from youtube
-» /song (query) - download song from youtube
-» /search (query) - search for the youtube video link
-» /ping - show the bot ping status
-» /uptime - show the bot uptime status
-» /alive - show the bot alive info (in Group only)""",
+» /play (nama lagu/link youtube) - memutar musik dari youtube
+» /stream (m3u8/youtube link live stream) - putar musik streaming langsung youtube/m3u8
+» /vplay (nama video/link youtube) - memutar video dari youtube
+» /vstream (m3u8/youtube link live stream) - putar video streaming langsung youtube/m3u8
+» /playlist - melihat daftar antrian lagu dan lagu yang sedang diputar
+» /lyric (nama lagu) - mencari lirik lagu berdasarkan nama lagu
+» /video (nama lagu / judul video) - unduh video dari youtube
+» /song (nama lagu) - download lagu dari youtube
+» /search (query) - cari link video youtube
+» /ping - tampilkan status bot ping
+» /uptime - tampilkan status uptime bot
+» /alive - tampilkan info bot hidup (hanya di Grup)""",
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("🔙 Go Back", callback_data="command_list")]]
+            [[InlineKeyboardButton("🔙 Kembali", callback_data="command_list")]]
         ),
     )
 
@@ -174,24 +164,24 @@ async def user_set(_, query: CallbackQuery):
 @Client.on_callback_query(filters.regex("admin_command"))
 @check_blacklist()
 async def admin_set(_, query: CallbackQuery):
-    await query.answer("admin commands")
+    await query.answer("Perintah Untuk Admin")
     await query.edit_message_text(
-        f"""✏️ Command list for group admin.
+        f"""✏️ Daftar perintah untuk admin grup.
 
-» /pause - pause the current track being played
-» /resume - play the previously paused track
-» /skip - goes to the next track
-» /stop - stop playback of the track and clears the queue
-» /vmute - mute the streamer userbot on group call
-» /vunmute - unmute the streamer userbot on group call
-» /volume `1-200` - adjust the volume of music (userbot must be admin)
-» /reload - reload bot and refresh the admin data
-» /userbotjoin - invite the userbot to join group
-» /userbotleave - order userbot to leave from group
-» /startvc - start/restart the group call
-» /stopvc - stop/discard the group call""",
+» /pause - menjeda trek yang sedang diputar
+» /resume - mainkan trek yang sebelumnya dijeda
+» /skip - pergi ke trek berikutnya
+» /stop - hentikan pemutaran trek dan bersihkan antrian
+» /vmute - bisukan assistant bot streamer pada panggilan grup
+» /vunmute - mengaktifkan suara assistant bot streamer pada panggilan grup
+» /volume `1-200` - mengatur volume musik (assistant bot harus admin)
+» /reload - reload bot dan refresh data admin
+» /userbotjoin - undang assistant bot untuk bergabung dengan grup
+» /userbotleave - perintahkan assistant bot keluar dari grup
+» /startvc - memulai/memulai ulang panggilan grup
+» /stopvc - hentikan/buang panggilan grup""",
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("🔙 Go Back", callback_data="command_list")]]
+            [[InlineKeyboardButton("🔙 Kembali", callback_data="command_list")]]
         ),
     )
 
@@ -201,24 +191,24 @@ async def admin_set(_, query: CallbackQuery):
 async def sudo_set(_, query: CallbackQuery):
     user_id = query.from_user.id
     if user_id not in SUDO_USERS:
-        await query.answer("⚠️ You don't have permissions to click this button\n\n» This button is reserved for sudo members of this bot.", show_alert=True)
+        await query.answer("⚠️ Anda tidak memiliki izin untuk mengklik tombol ini\n\n» Tombol ini disediakan untuk anggota sudo bot ini.", show_alert=True)
         return
     await query.answer("sudo commands")
     await query.edit_message_text(
-        f"""✏️ Command list for sudo user.
+        f"""✏️ Daftar perintah untuk pengguna sudo.
 
-» /stats - get the bot current statistic
-» /calls - show you the list of all active group call in database
-» /block (`chat_id`) - use this to blacklist any group from using your bot
-» /unblock (`chat_id`) - use this to whitelist any group from using your bot
-» /blocklist - show you the list of all blacklisted chat
-» /speedtest - run the bot server speedtest
-» /sysinfo - show the system information
-» /logs - generate the current bot logs
-» /eval - run an code
-» /sh - run an code""",
+» /stats - dapatkan statistik bot saat ini
+» /calls - menampilkan daftar semua panggilan grup yang aktif di database
+» /block (`chat_id`) - gunakan ini untuk memasukan daftar block grup mana pun agar tidak menggunakan bot Anda
+» /unblock (`chat_id`) - gunakan ini untuk menghapus grup dari daftar block agar bisa menggunakan bot Anda
+» /blocklist - menampilkan daftar semua grup yang masuk daftar block
+» /speedtest - jalankan speedtest server bot
+» /sysinfo - tampilkan informasi sistem
+» /logs - buat log bot saat ini
+» /eval - menjalankan kode
+» /sh - menjalankan kode""",
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("🔙 Go Back", callback_data="command_list")]]
+            [[InlineKeyboardButton("🔙 Kembali", callback_data="command_list")]]
         ),
     )
 
@@ -228,22 +218,22 @@ async def sudo_set(_, query: CallbackQuery):
 async def owner_set(_, query: CallbackQuery):
     user_id = query.from_user.id
     if user_id not in OWNER_ID:
-        await query.answer("⚠️ You don't have permissions to click this button\n\n» This button is reserved for owner of this bot.", show_alert=True)
+        await query.answer("⚠️ Anda tidak memiliki izin untuk mengklik tombol ini\n\n» Tombol ini disediakan untuk pemilik bot ini.", show_alert=True)
         return
     await query.answer("owner commands")
     await query.edit_message_text(
-        f"""✏️ Command list for bot owner.
+        f"""✏️ Daftar perintah untuk pemilik bot.
 
-» /gban (`username` or `user_id`) - for global banned people, can be used only in group
-» /ungban (`username` or `user_id`) - for un-global banned people, can be used only in group
-» /update - update your bot to latest version
-» /restart - restart your bot server
-» /leaveall - order userbot to leave from all group
-» /leavebot (`chat id`) - order bot to leave from the group you specify
-» /broadcast (`message`) - send a broadcast message to all groups in bot database
-» /broadcast_pin (`message`) - send a broadcast message to all groups in bot database with the chat pin""",
+» /gban (`username` atau `user_id`) - untuk orang yang diblokir secara global, hanya dapat digunakan dalam grup
+» /ungban (`username` atau `user_id`) - untuk orang yang dilarang secara global, hanya dapat digunakan dalam grup
+» /update - perbarui bot Anda ke versi terbaru
+» /restart - restart server bot Anda
+» /leaveall - perintahkan assistant bot keluar dari semua grup
+» /leavebot (`chat id`) - perintahkan bot untuk keluar dari grup yang Anda tentukan
+» /broadcast (`pesan`) - mengirim pesan broadcast ke semua grup di database bot
+» /broadcast_pin (`pesan`) - mengirim pesan siaran ke semua grup di database bot dengan pin obrolan""",
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("🔙 Go Back", callback_data="command_list")]]
+            [[InlineKeyboardButton("🔙 Kembali", callback_data="command_list")]]
         ),
     )
 
@@ -254,7 +244,7 @@ async def at_set_markup_menu(_, query: CallbackQuery):
     user_id = query.from_user.id
     a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
     if not a.can_manage_voice_chats:
-        return await query.answer("💡 Only admin with manage video chat permission that can tap this button !", show_alert=True)
+        return await query.answer("💡 Hanya admin dengan izin mengelola obrolan video yang dapat mengetuk tombol ini !", show_alert=True)
     chat_id = query.message.chat.id
     user_id = query.message.from_user.id
     buttons = menu_markup(user_id)
@@ -262,7 +252,7 @@ async def at_set_markup_menu(_, query: CallbackQuery):
         await query.answer("control panel opened")
         await query.edit_message_reply_markup(reply_markup=InlineKeyboardMarkup(buttons))
     else:
-        await query.answer("❌ nothing is currently streaming", show_alert=True)
+        await query.answer("❌ tidak ada yang sedang streaming", show_alert=True)
 
 
 @Client.on_callback_query(filters.regex("stream_home_panel"))
@@ -270,7 +260,7 @@ async def at_set_markup_menu(_, query: CallbackQuery):
 async def is_set_home_menu(_, query: CallbackQuery):
     a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
     if not a.can_manage_voice_chats:
-        return await query.answer("💡 Only admin with manage video chat permission that can tap this button !", show_alert=True)
+        return await query.answer("💡 Hanya admin dengan izin mengelola obrolan video yang dapat mengetuk tombol ini !", show_alert=True)
     await query.answer("control panel closed")
     user_id = query.message.from_user.id
     buttons = stream_markup(user_id)
@@ -282,7 +272,7 @@ async def is_set_home_menu(_, query: CallbackQuery):
 async def on_close_menu(_, query: CallbackQuery):
     a = await _.get_chat_member(query.message.chat.id, query.from_user.id)
     if not a.can_manage_voice_chats:
-        return await query.answer("💡 Only admin with manage video chat permission that can tap this button !", show_alert=True)
+        return await query.answer("💡 Hanya admin dengan izin mengelola obrolan video yang dapat mengetuk tombol ini !", show_alert=True)
     await query.message.delete()
 
 
