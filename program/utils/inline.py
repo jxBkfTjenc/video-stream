d93e8dc1bb6f816eda5d41b48c3204a4582241d4
jxBkfTjenc <99679586+jxBkfTjenc@users.noreply.github.com>
2022-04-03 19:16:23 +0700

    Update inline.py

--- program/utils/inline.py
+++ program/utils/inline.py
@@ -10,8 +10,8 @@ from pyrogram.types import (
 def stream_markup(user_id):
   buttons = [
     [
-      InlineKeyboardButton(text="• Mᴇɴᴜ", callback_data=f'stream_menu_panel | {user_id}'),
-      InlineKeyboardButton(text="• Cʟᴏsᴇ", callback_data=f'set_close'),
+      InlineKeyboardButton(text="• Menu", callback_data=f'stream_menu_panel | {user_id}'),
+      InlineKeyboardButton(text="• Tutup", callback_data=f'set_close'),
     ],
   ]
   return buttons
@@ -30,7 +30,7 @@ def menu_markup(user_id):
       InlineKeyboardButton(text="🔊", callback_data=f'set_unmute | {user_id}'),
     ],
     [
-      InlineKeyboardButton(text="🔙 Go Back", callback_data='stream_home_panel'),
+      InlineKeyboardButton(text="🔙 Kembali", callback_data='stream_home_panel'),
     ]
   ]
   return buttons
@@ -40,7 +40,7 @@ close_mark = InlineKeyboardMarkup(
   [
     [
       InlineKeyboardButton(
-        "🗑 Close", callback_data="set_close"
+        "🗑 Tutup", callback_data="set_close"
       )
     ]
   ]
@@ -51,7 +51,7 @@ back_mark = InlineKeyboardMarkup(
   [
     [
       InlineKeyboardButton(
-        "🔙 Go Back", callback_data="stream_menu_panel"
+        "🔙 Kembali", callback_data="stream_menu_panel"
       )
     ]
   ]
